60cf94bd61935f42ae292639095939671a6c6290
Dmitry Leschik <fpm.leschik@bsu.by>
2022-11-10 00:01:27 +0300

    win threads

--- lab2-easy-matrix/MatrixMultiplier.cpp
+++ lab2-easy-matrix/MatrixMultiplier.cpp
@@ -1,13 +1,14 @@
 #include "MatrixMultiplier.h"
 
-pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+std::mutex lock;
 
 std::vector<std::vector<int>> a;
 std::vector<std::vector<int>> b;
 int blockSize;
 std::vector<std::vector<int>> out;
 
-static void *multiplyBlocks(void* args);
+DWORD WINAPI multiplyBlocks(LPVOID args);
+HANDLE* vectorToArray(std::vector<HANDLE> threads);
 
 std::vector<std::vector<int>> MatrixMultiplier::multiplyMatrices(std::vector<std::vector<int>>& _a, std::vector<std::vector<int>>& _b, int _blockSize, bool async) {
 
@@ -17,13 +18,7 @@ std::vector<std::vector<int>> MatrixMultiplier::multiplyMatrices(std::vector<std
 
     out = std::vector<std::vector<int>>(_a.size(), std::vector<int>(_a.size()));
 
-    std::vector<pthread_t> threads;
-    pthread_attr_t attr;
-
-    if (async) {
-        pthread_mutex_init(&lock, NULL);
-        pthread_attr_init(&attr);
-    }
+    std::vector<HANDLE> threads;
 
     for (int line = 0; line < a.size(); line += blockSize) {
         for (int row = 0; row < a.size(); row += blockSize) {
@@ -32,8 +27,11 @@ std::vector<std::vector<int>> MatrixMultiplier::multiplyMatrices(std::vector<std
             args[1] = row;
 
             if (async) {
-                threads.emplace_back();
-                pthread_create(&threads.back(), &attr, &multiplyBlocks, args);
+                DWORD i;
+                threads.push_back(CreateThread(NULL, 0, multiplyBlocks, args, 0, &i));
+                if (i == NULL){
+                    ExitProcess(3);
+                }
             }
             else {
                 multiplyBlocks(args);
@@ -41,27 +39,39 @@ std::vector<std::vector<int>> MatrixMultiplier::multiplyMatrices(std::vector<std
         }
     }
     if (async) {
-        for (pthread_t &thread: threads) {
-            pthread_join(thread, nullptr);
+        WaitForMultipleObjects(threads.size(), vectorToArray(threads) , TRUE, INFINITE);
+
+        for (auto &item: threads){
+            CloseHandle(item);
         }
     }
     return out;
 }
 
+HANDLE* vectorToArray(std::vector<HANDLE> threads){
+    auto* array = new HANDLE[threads.size()];
 
-void *multiplyBlocks(void* args) {
+    for (int i = 0; i < threads.size(); i++) {
+        array[i] = threads[i];
+    }
+    return array;
+}
+
+
+DWORD WINAPI multiplyBlocks(LPVOID args) {
     int* nums = static_cast<int *>(args);
     int line = nums[0];
     int row = nums[1];
 
-    for (int i = line; i < std::min(line + blockSize, (int)a.size(), [](int a1, int b1) {return a1 < b1;}); ++i) {
-        for (int j = row; j < std::min(row + blockSize, (int)a.size(), [](int a2, int b2) {return a2 < b2;}); ++j) {
+    for (int i = line; i < min(line + blockSize, (int)a.size()); ++i) {
+        for (int j = row; j < min(row + blockSize, (int) a.size()); ++j) {
             for (int k = 0; k < a.size(); ++k) {
-                pthread_mutex_lock(&lock);
+                lock.lock();
                 out[i][j] += a[i][k] * b[k][j];
-                pthread_mutex_unlock(&lock);
+                lock.unlock();
             }
         }
     }
     delete nums;
+    return 0;
 }
