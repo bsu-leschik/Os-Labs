32fd924fe8a5687383f635118da10d027a756592
bsu-leschik <pimako2003@yandex.by>
2022-12-08 11:01:50 +0300

    did win pipes

--- lab5-pipe/windows/main.cpp
+++ lab5-pipe/windows/main.cpp
@@ -1,4 +1,5 @@
 #include <windows.h>
+
 #include <namedpipeapi.h>
 #include <fileapi.h>
 
@@ -40,18 +41,37 @@ void configureStartupInfo(HANDLE descriptors[2]){
 int main(){
     HANDLE descriptors[2];
 
+    si.hStdInput = GetStdHandle(STD_INPUT_HANDLE);
+    si.hStdError = GetStdHandle(STD_ERROR_HANDLE);
+    si.dwFlags = STARTF_USESTDHANDLES;
+
     for (int i = 0; i < 3; ++i) {
         openPipe(descriptors);
         PROCESS_INFORMATION process_information;
-        configureStartupInfo(descriptors);
-        CreateProcess(nullptr, processes[0].data(), nullptr, nullptr, TRUE, 0, nullptr, nullptr, &si,
+
+        ZeroMemory( &process_information, sizeof(process_information) );
+
+        si.hStdOutput = descriptors[1];
+
+        CreateProcess(nullptr, processes[i].data(), nullptr, nullptr, TRUE, 0, nullptr, nullptr, &si,
                       &process_information);
-        closePipe(descriptors);
+
+        CloseHandle(descriptors[1]);
         WaitForSingleObject(process_information.hProcess, INFINITE);
         WaitForSingleObject(process_information.hThread, INFINITE);
         CloseHandle(process_information.hThread);
         CloseHandle(process_information.hProcess);
+        si.hStdInput = descriptors[0];
     }
+
+    PROCESS_INFORMATION process_information;
+    si.hStdOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+    CreateProcess(nullptr, processes[3].data(), nullptr, nullptr, TRUE, 0, nullptr, nullptr, &si,
+                  &process_information);
+    WaitForSingleObject(process_information.hProcess, INFINITE);
+    WaitForSingleObject(process_information.hThread, INFINITE);
+    CloseHandle(process_information.hThread);
+    CloseHandle(process_information.hProcess);
 
     return 0;
 }
