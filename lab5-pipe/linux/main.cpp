54429f2b582b91de8339746c8c42856bbe193281
Dmitry Leschik <dmitry.leschik@gmail.com>
2023-12-03 19:08:05 +0300

    fixed execl

--- lab5-pipe/linux/main.cpp
+++ lab5-pipe/linux/main.cpp
@@ -13,23 +13,24 @@ int main(){
     int pipefd[2];
 
     for (int i = 0; i < 3; ++i) {
+
         pipe(pipefd);
 
         if(fork() == 0){
+            close(pipefd[0]);
             dup2(pipefd[1], STDOUT_FILENO);
-
-            execl(processes[i].c_str(), "");
-            std::exit(0);
+            execl(processes[i].c_str(), "", nullptr);
+            exit(0);
         }
-        else{
+        else {
             close(pipefd[1]);
-            wait(nullptr);
             dup2(pipefd[0], STDIN_FILENO);
+            wait(nullptr);
         }
     }
 
     if (fork() == 0) {
-        execl(processes[3].c_str(), "");
+        execl(processes[3].c_str(), "", nullptr);
         exit(0);
     }
     wait(nullptr);
